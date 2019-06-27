365b7b13848f4968ed6c00ac81aaca69038e3416
William D. Jones <thor0505@comcast.net>
2019-06-27 08:57:46 -0400

    Show that Case also accepts integers.

--- 7seg_count/7seg_count.py
+++ 7seg_count/7seg_count.py
@@ -87,37 +87,37 @@ class DigitToSegments(Elaboratable):
         m = Module()
         
         with m.Switch(self.digit):
-            with m.Case("0000"):
+            with m.Case(0x0):
                 m.d.sync += self.segments.eq(0b0111111)
-            with m.Case("0001"):
+            with m.Case(0x1):
                 m.d.sync += self.segments.eq(0b0000110)
-            with m.Case("0010"):
+            with m.Case(0x2):
                 m.d.sync += self.segments.eq(0b1011011)
-            with m.Case("0011"):
+            with m.Case(0x3):
                 m.d.sync += self.segments.eq(0b1001111)
-            with m.Case("0100"):
+            with m.Case(0x4):
                 m.d.sync += self.segments.eq(0b1100110)
-            with m.Case("0101"):
+            with m.Case(0x5):
                 m.d.sync += self.segments.eq(0b1101101)
-            with m.Case("0110"):
+            with m.Case(0x6):
                 m.d.sync += self.segments.eq(0b1111101)
-            with m.Case("0111"):
+            with m.Case(0x7):
                 m.d.sync += self.segments.eq(0b0000111)
-            with m.Case("1000"):
+            with m.Case(0x8):
                 m.d.sync += self.segments.eq(0b1111111)
-            with m.Case("1001"):
+            with m.Case(0x9):
                 m.d.sync += self.segments.eq(0b1101111)
-            with m.Case("1010"):
+            with m.Case(0xA):
                 m.d.sync += self.segments.eq(0b1111100)
-            with m.Case("1011"):
+            with m.Case(0xB):
                 m.d.sync += self.segments.eq(0b1111100)
-            with m.Case("1100"):
+            with m.Case(0xC):
                 m.d.sync += self.segments.eq(0b0111001)
-            with m.Case("1101"):
+            with m.Case(0xD):
                 m.d.sync += self.segments.eq(0b1011110)
-            with m.Case("1110"):
+            with m.Case(0xE):
                 m.d.sync += self.segments.eq(0b1111001)
-            with m.Case("1111"):
+            with m.Case(0xF):
                 m.d.sync += self.segments.eq(0b1110001)
         
         return m
